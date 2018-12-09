1e57ae0dcfafa11dd981169aca75e906d933e6a6
murreiHD <sokol-vitek@mail.ru>
2018-12-09 20:09:42 +0300

    Add delete Post in API

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,90 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Load post Validator
+const validatePostInput = require("../../validation/post");
+
+// Post model
+const Profile = require("../../models/Profile");
+
+// Profile model
+const Post = require("../../models/Post");
 
 // @route   GET api/posts/test
 // @desc    Tests post route
 // @access  Public
-router.get('/test', (req, res) => res.json({ msg: 'Posts Works' }));
+router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
+
+// @route   GET api/posts/
+// @desc    Get posts
+// @access  Public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ noposts: "no posts found" }));
+});
+
+// @route   GET api/posts/:id
+// @desc    Get posts by id
+// @access  Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err => res.status(404).json({ nopost: "no post found" }));
+});
+
+// @route   POST api/posts/
+// @desc    Add post
+// @access  Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // If any errors with errors object
+      return res.status(400).json(errors);
+    }
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
+  }
+);
+
+// @route   DELETE api/posts/:id
+// @desc    Delete post
+// @access  Private
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          // Check for post owner
+          if (post.user.toString() !== req.user.id) {
+            return res
+              .status(401)
+              .json({ notauthorized: "User nor autherize" });
+          }
+
+          //DElete
+          post.remove().then(() => res.json({ success: true }));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "Not post " }));
+    });
+  }
+);
 
 module.exports = router;
